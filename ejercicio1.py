bce126e2f0cfbfdaac17c88faf263c57c1bca519
DevilOpie <148334046+DevilOpie@users.noreply.github.com>
2023-11-07 10:20:45 +0100

    Archivo de python
    Ejercicio 1 upload

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+print("Cual es tu nombre")
+name = input()
+
+print("Cual es tu edad")
+age = input()
+ageInt= int(age)
+days = (ageInt * 365)
+print("Tu nombre es:",name,"y has vivido",days,"dias")
